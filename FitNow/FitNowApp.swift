8ca472b81d7e0888336ab346bf08052616269fdf
Manuel Cosovschi <cosovschim@gmail.com>
2025-08-26 13:21:54 -0300

    Initial Commit

---
+++ FitNow/FitNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitNowApp.swift
+//  FitNow
+//
+//  Created by Manuel Cosovschi on 26/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FitNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitNowTests/FitNowTests.swift
@@ -0,0 +1,16 @@
+//
+//  FitNowTests.swift
+//  FitNowTests
+//
+//  Created by Manuel Cosovschi on 26/8/25.
+//
+
+import Testing
+
+struct FitNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
